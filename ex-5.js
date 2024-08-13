f0b3004d1e7f0457db8353bf284b21177f30ac68
Pattaradanai Chabang <ktop@iMac-khxng-Pattaradanai.local>
2024-08-13 14:50:20 +0700

    Ex 1-8 Finish

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let TotalProductPrices = 27000;
+let discount = (TotalProductPrices * 0.25);
+let realPrice = (TotalProductPrices - discount);
+console.log(realPrice);
